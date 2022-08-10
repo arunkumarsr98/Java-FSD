7788597d26475afe39b51af89e2380f8893bb7e5
Arun Kumar <82430427+arunkumarsr98@users.noreply.github.com>
2022-08-10 11:14:24 +0530

    Add files via upload

---
+++ ph2_practice/src/login/LoginValidation.java
@@ -0,0 +1,55 @@
+package login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginValidation
+ */
+//@WebServlet("/LoginValidation")
+public class LoginValidation extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginValidation() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter print=response.getWriter();
+		String user=request.getParameter("user");
+		String pass=request.getParameter("pass");
+		String user1="Arun";
+		String user2="pass";
+		if(user.equals(user1) && pass.equals(user2)) {
+			print.print("login sucessful");
+		}
+		else {
+			print.print("login failed");
+		}
+
+		doGet(request, response);
+	}
+
+}
