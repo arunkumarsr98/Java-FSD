1b98cd466230c2bb8d0614ee30cf720c7c20793a
Arun Kumar <82430427+arunkumarsr98@users.noreply.github.com>
2022-09-29 20:57:34 +0530

    Delete Phase4_Practice/MyPortfolio directory

--- Phase4_Practice/MyPortfolio/build&deploy-github.bat
+++
@@ -1,3 +0,0 @@
-call ng build --prod --base-href "https://lenzoburger.github.io/MyPortfolio/"
-call npx ngh --dir=dist/MyPortfolio --message="Auto commit & deploy"
-call echo Successfully built and deployed!
